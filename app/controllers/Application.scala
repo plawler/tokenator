1073e760987ebfd627fa9669dcc54dc59acfd4b5
Paul Lawler <paullawler@Pauls-MacBook-Pro-2.local>
2015-08-25 17:17:09 -0400

    removed some sample heroku code

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,9 +8,6 @@ import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits._
 
 import com.google.inject.Inject
-import org.jscience.physics.amount.Amount
-import org.jscience.physics.model.RelativisticModel
-import javax.measure.unit.SI.KILOGRAM
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
@@ -22,13 +19,6 @@ class Application @Inject() (tokenService: StormPathTokenService) extends Contro
     Ok("Your new application is ready.")
   }
 
-  def science = Action {
-    RelativisticModel.select()
-    val m = Amount.valueOf("12 GeV").to(KILOGRAM)
-    val testRelativity = s"E=mc^2: 12 GeV = $m"
-    Ok(testRelativity)
-  }
-
   def token(application: String) = Action.async { implicit request =>
     tokenService.retrieveToken(request)(application) match {
       case Success(futureToken) => futureToken.map(token => Ok(Json.toJson(token)))
@@ -37,5 +27,7 @@ class Application @Inject() (tokenService: StormPathTokenService) extends Contro
         Future.successful(Unauthorized)
     }
   }
+
+  def refresh = TODO
 
 }
